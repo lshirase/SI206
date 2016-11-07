470507972c6dc3e69d538afc3d7405819eedf871
Len Shirase <lshirase@umich.edu>
2016-11-07 14:51:37 -0500

    no pw

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -7,10 +7,7 @@ import tweepy
 
 
 # Unique code from Twitter
-access_token = "16803203-FgMMK3RGdTjwneE2L7n1V64wSvzHOuCkvHI0YhWn8"
-access_token_secret = "aUJzuuYtfGRx9uZZTQr95b7FR9DEdjFZyAZyRkHX9yVBd"
-consumer_key = "2ovZ0iyuKvAYTnwRUbdq3Swqn"
-consumer_secret = "w7BjX5omv5T2rvU8CRTIYCXkvis41DbujrwWRcJ6Lzxa5TkDHt"
+
 
 # Boilerplate code here
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
