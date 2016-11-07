230607bbcb1d95b6ae1f8d1005674e3236cbff89
Len Shirase <lshirase@umich.edu>
2016-11-07 14:50:55 -0500

    no pw

--- twitterhw3b.py
+++ twitterhw3b.py
@@ -11,10 +11,7 @@ from textblob import TextBlob
 
 
 # Unique code from Twitter
-access_token = "16803203-FgMMK3RGdTjwneE2L7n1V64wSvzHOuCkvHI0YhWn8"
-access_token_secret = "aUJzuuYtfGRx9uZZTQr95b7FR9DEdjFZyAZyRkHX9yVBd"
-consumer_key = "2ovZ0iyuKvAYTnwRUbdq3Swqn"
-consumer_secret = "w7BjX5omv5T2rvU8CRTIYCXkvis41DbujrwWRcJ6Lzxa5TkDHt"
+
 
 # Boilerplate code here
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
