317204bc5d5544f6df7d9e48b20bfd64257de252
Len Shirase <lshirase@umich.edu>
2016-11-06 20:42:56 -0500

    almost figured out madlibhw3

--- madlibhw3.py
+++ madlibhw3.py
@@ -11,5 +11,50 @@
 # 1) Print the new text
 print("START*******")
 
+import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+import random
+from nltk import word_tokenize,sent_tokenize
+from nltk.book import * 
+nltk.download('punkt')
+
+
+
+tokens = text2[0:150]
+print("TOKENS")
+print(tokens)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+print("TAGGED TOKENS")
+print(tagged_tokens)
+print(len(tagged_tokens))
+print(len(tokens))
+
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
+substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+
+def spaced(word):
+	if word in ["[", "]", ",", ".", "?", "!", ":", ";"]:
+		return word
+	else:
+		return " " + word
+
+print("ORGINAL TEXT")
+for word in tokens:
+	print(spaced(word), end="")
+print(" ".join(tokens))
+
+
+final_words = []
+
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
+
 
 print("\n\nEND*******")
